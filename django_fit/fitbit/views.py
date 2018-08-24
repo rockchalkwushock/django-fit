5811e031fbc5e8c3adfe751283a7f48bcabe995c
Cody Brunner <rockchalkwushock@icloud.com>
2018-08-24 14:23:51 -0700

    removes Profile model and serializer.

--- django_fit/fitbit/views.py
+++ django_fit/fitbit/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+class UserList(generics.ListAPIView):  # new
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):  # new
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
