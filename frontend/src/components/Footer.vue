5d101fa4349795877b0a53a740c386add1169fd5
Cody Brunner <rockchalkwushock@icloud.com>
2018-09-04 14:15:12 -0700

    removes HelloWorld and assets, adds Header & Footer.

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer>
+    <p v-if="date > 2018">© 2018 - {{date}} Cody Brunner</p>
+    <p v-else>© {{date}} Cody Brunner</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'App-Footer',
+  data() {
+    return {
+      date: new Date().getFullYear()
+    }
+  }
+}
+</script>
+
+<style>
+footer {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+</style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Django Fit</h1>
+</template>
+
+<style>
+h1 {
+  color: mediumseagreen;
+  font-size: 5rem;
+}
+</style>
+
