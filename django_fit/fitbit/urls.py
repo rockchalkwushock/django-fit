03a950fb55a1dbd467852f6654d42dafddf90412
Cody Brunner <rockchalkwushock@icloud.com>
2018-09-10 10:39:34 -0700

    updates data returned from API requests, removes djangorestframework.

--- django_fit/fitbit/urls.py
+++ django_fit/fitbit/urls.py
@@ -2,15 +2,9 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView
 from . import views
 
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register('fitbit', views.UserList)
-
 app_name = 'fitbit'
 urlpatterns = [
     path('', TemplateView.as_view(template_name='fitbit/index.html')),
-    path('api/', include(router.urls), name='api'),
     path('api/get_today', views.get_today, name='today'),
     path('api/get_last_week', views.get_last_week, name='last_week'),
     path('api/get_profile', views.get_profile, name='profile')
--- django_fit/fitbit/views.py
+++ django_fit/fitbit/views.py
@@ -1,17 +1,8 @@
-from django.contrib.auth.models import User
-from rest_framework import generics, viewsets
 from requests import get
 from json import dumps
 from social_django.utils import load_strategy
 from django.http import HttpResponse
 from datetime import datetime, timedelta
-
-from .serializers import UserSerializer
-
-
-class UserList(viewsets.ModelViewSet):  # new
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
 
 
 def get_today(request):
@@ -28,37 +19,20 @@ def get_today(request):
     if response['summary'].get('heartRateZones', None):
         data = {
             'calories_out': response['summary']['caloriesOut'],
-            'calories_bmr': response['summary']['caloriesBMR'],
             'distance': response['summary']['distances'][0]['distance'],
             'floors': response['summary']['floors'],
             'heart_rate': [
                 {
                     'calories_out': round(response['summary']['heartRateZones'][0]['caloriesOut'], 0),
-                    'max': response['summary']['heartRateZones'][0]['max'],
-                    'min': response['summary']['heartRateZones'][0]['min'],
-                    'minutes': response['summary']['heartRateZones'][0]['minutes'],
-                    'name': 'Out of Range',
                 },
                 {
                     'calories_out': round(response['summary']['heartRateZones'][1]['caloriesOut'], 0),
-                    'max': response['summary']['heartRateZones'][1]['max'],
-                    'min': response['summary']['heartRateZones'][1]['min'],
-                    'minutes': response['summary']['heartRateZones'][1]['minutes'],
-                    'name': 'Fat burn',
                 },
                 {
                     'calories_out': round(response['summary']['heartRateZones'][2]['caloriesOut'], 0),
-                    'max': response['summary']['heartRateZones'][2]['max'],
-                    'min': response['summary']['heartRateZones'][2]['min'],
-                    'minutes': response['summary']['heartRateZones'][2]['minutes'],
-                    'name': 'Cardio',
                 },
                 {
                     'calories_out': round(response['summary']['heartRateZones'][3]['caloriesOut'], 0),
-                    'max': response['summary']['heartRateZones'][3]['max'],
-                    'min': response['summary']['heartRateZones'][3]['min'],
-                    'minutes': response['summary']['heartRateZones'][3]['minutes'],
-                    'name': 'Peak',
                 }
             ],
             'steps': response['summary']['steps']
@@ -66,7 +40,6 @@ def get_today(request):
     else:
         data = {
             'calories_out': response['summary']['caloriesOut'],
-            'calories_bmr': response['summary']['caloriesBMR'],
             'distance': response['summary']['distances'][0]['distance'],
             'floors': response['summary']['floors'],
             'steps': response['summary']['steps']
